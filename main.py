d7196b932999539397dd0b0a125d7dd0850a9b3f
User <sanjarbek.89.ss@gmail.com>
2023-05-05 14:49:45 +0500

    add discription image and links

--- main.py
+++ main.py
@@ -18,14 +18,20 @@ He teaches Customs statistics and system analysis at the Customs Institute """
 content2= """Bellow you can find some of the apps I have build in Python. Feel free to contact me!"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 print(df)
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write("[Source Code](https://python.org)")
 
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")

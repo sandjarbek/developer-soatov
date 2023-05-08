1073490bf60cdfb7fbb48984cc9ee8df47ed2475
User <sanjarbek.89.ss@gmail.com>
2023-05-09 03:26:05 +0500

    send email useing the form

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -3,11 +3,18 @@ from send_email import send_email
 
 st.header("Contact me")
 
-with st.form(key="Email forms"):
+with st.form(key="email forms"):
     user_gmail= st.text_input("Your email address")
-    message =st.text_area("Your message")
-    message=message+"\n"+user_gmail
+    raw_message =st.text_area("Your message")
+    message=f"""\
+Subject: New eamil from {user_gmail}
+From {user_gmail}
+{raw_message}
+"""
+
     button = st.form_submit_button("Submit")
+    print(button)
 
     if button:
-         send_email(message, user_gmail)
+         send_email(message)
+         st.info("Your email was send successfully")
--- send_email.py
+++ send_email.py
@@ -1,18 +1,22 @@
 import smtplib, ssl
 
 
-def send_email(message, user_gmail):
+def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     password = "wkhiaexeauqdlhlb"
     # user = "securesally@gmail.com"
     user = "sanjarbek.soatov1989@gmail.com"
-    # receiver = "sanjarbek.89.ss@gmail.com"
+    receiver = "sanjarbek.89.ss@gmail.com"
     context = ssl.create_default_context()
+
+
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(user, password)
-        server.sendmail(user, user_gmail, message)
+        server.sendmail(user, receiver, message)
+
 
 
 # send_email(message)
+

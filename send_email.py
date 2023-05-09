a0819f118d47585c53450b845172aec0bd6cd49a
User <sanjarbek.89.ss@gmail.com>
2023-05-09 12:11:05 +0500

    password changed

--- send_email.py
+++ send_email.py
@@ -1,10 +1,20 @@
 import smtplib, ssl
+import os
+
+# if (os.environ.get("DB_TYPE") == None):
+#  from dotenv import load_dotenv
+#  from config.definitions import ROOT_DIR
+#  load_dotenv(os.path.join(ROOT_DIR, 'config', 'conf', '.env'))
+from dotenv import load_dotenv
+load_dotenv()
+
+password = os.getenv("PASSWORD")
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    password = "wkhiaexeauqdlhlb"
+    password = os.getenv("PASSWORD")
     # user = "securesally@gmail.com"
     user = "sanjarbek.soatov1989@gmail.com"
     receiver = "sanjarbek.89.ss@gmail.com"
@@ -20,3 +30,5 @@ def send_email(message):
 
 # send_email(message)
 
+if __name__=='__main__':
+    print(password)
